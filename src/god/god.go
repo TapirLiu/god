5e0eb892c6961a3e8df655783320ff042fbe1232
Alexander Fahlke <alexander.fahlke@gfk.com>
2015-09-17 14:25:32 +0200

    fixed indentation (now go fmt conform)

--- src/god/god.go
+++ src/god/god.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-   "os"
-   "os/exec"
-   "path/filepath"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
-func main () {
-   gopath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-   os.Setenv ("GOPATH", gopath)
-   cmd := exec.Command("go", os.Args [1:]...)
-   cmd.Stdin = os.Stdin
-   cmd.Stdout = os.Stdout
-   cmd.Stderr = os.Stderr
-   cmd.Run ()
+func main() {
+	gopath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	os.Setenv("GOPATH", gopath)
+	cmd := exec.Command("go", os.Args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
